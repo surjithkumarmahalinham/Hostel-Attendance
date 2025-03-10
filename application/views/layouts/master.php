50065a126ef93d8e90fc4453d9cfcadfbd47ad2f
Your Name <msksurjith@gmail.com>
2025-03-10 10:25:17 +0530

    master module changes

---
+++ application/views/layouts/master.php
@@ -0,0 +1,50 @@
+<?php include('header.php'); ?>
+<body data-headerbg="color_10" data-sibebarbg="color_10">
+
+    <!--*******************
+        Preloader start
+    ********************-->
+    <div id="preloader">
+        <div class="gooey">
+		  <span class="dot"></span>
+		  <div class="dots">
+			<span></span>
+			<span></span>
+			<span></span>
+		  </div>
+		</div>
+    </div>
+    <!--*******************
+        Preloader end
+    ********************-->
+
+    <!--**********************************
+        Main wrapper start
+    ***********************************-->
+    <div id="main-wrapper">
+
+    <!--**********************************
+            Nav header start
+        ***********************************-->
+        <div class="nav-header">
+        <a href="index.html" class="brand-logo">
+                <span class="brand-title" width="165" height="35" viewBox="0 0 165 35" fill="none" xmlns="http://www.w3.org/2000/svg">
+					Hostel
+</span>
+
+
+            </a>
+            <div class="nav-control">
+                <div class="hamburger">
+                    <span class="line"></span><span class="line"></span><span class="line"></span>
+                </div>
+            </div>
+        </div>
+        <?php include('topbar.php'); ?>
+        <?php include('chat_pop.php'); ?>
+        <?php include('sidebar.php'); ?>
+        
+        
+
+</body>
+</html>
