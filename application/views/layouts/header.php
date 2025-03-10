a2875f51991db4ca27a08a78484b1d855813c23f
Your Name <msksurjith@gmail.com>
2025-03-10 10:25:05 +0530

    header module changes

---
+++ application/views/layouts/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="keywords" content="" />
+	<meta name="author" content="" />
+	<meta name="robots" content="" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="" />
+	<meta property="og:title" content="" />
+	<meta property="og:description" content="" />
+	<meta property="og:image" content="social-image.png" />
+	<meta name="format-detection" content="telephone=no">
+	
+	<!-- PAGE TITLE HERE -->
+	<title>Hostel</title>
+	
+	<!-- FAVICONS ICON -->
+	<link rel="shortcut icon" type="image/png" href="<?php echo base_url('images/favicon.png'); ?>" />
+	<link rel="stylesheet" href="<?php echo base_url('public/assets/vendor/chartist/css/chartist.min.css'); ?>">
+	<link href="<?php echo base_url('public/assets/vendor/jquery-nice-select/css/nice-select.css'); ?>" rel="stylesheet">
+	<!-- Style css -->
+	<link href="<?php echo base_url('public/assets/vendor/datatables/css/jquery.dataTables.min.css');?>" rel="stylesheet">
+    <link href="<?php echo base_url('public/assets/css/style.css'); ?>" rel="stylesheet">
+	
+</head>
