883609f6ee7f317eaf8864be8f02975a3f0a0ec0
Your Name <msksurjith@gmail.com>
2025-03-10 10:24:50 +0530

    footer module changes

---
+++ application/views/layouts/footer.php
@@ -0,0 +1,36 @@
+<!--**********************************
+            Footer start
+        ***********************************-->
+        <div class="footer">
+		
+            <div class="copyright">
+                <p>School Management.</p>
+            </div>
+        </div>
+    
+
+
+	</div>
+  
+    <!-- Required vendors -->
+    <script src="<?php echo base_url('public/assets/vendor/global/global.min.js'); ?>"></script>
+	<script src="<?php echo base_url('public/assets/vendor/chart.js/Chart.bundle.min.js'); ?>"></script>
+	<script src="<?php echo base_url('public/assets/vendor/jquery-nice-select/js/jquery.nice-select.min.js'); ?>"></script>
+	
+	
+	<!-- Apex Chart -->
+	<script src="<?php echo base_url('public/assets/vendor/apexchart/apexchart.js'); ?>"></script>
+	
+	<!-- Chart piety plugin files -->
+    <script src="<?php echo base_url('public/assets/vendor/peity/jquery.peity.min.js'); ?>"></script>
+	
+	<!-- Dashboard 1 -->
+	<script src="<?php echo base_url('public/assets/js/dashboard/dashboard-1.js'); ?>"></script>
+
+    <script src="<?php echo base_url('public/assets/js/custom.min.js'); ?>"></script>
+	<script src="<?php echo base_url('public/assets/js/deznav-init.js'); ?>"></script>
+	<script src="<?php echo base_url('public/assets/js/demo.js'); ?>"></script>
+	
+	<!-- </script> -->
+</body>
+</html>
