b430ec42bb749d1c80a81a28b60f05848c4f3814
Your Name <msksurjith@gmail.com>
2025-03-11 18:19:26 +0530

    sidebar changes

---
+++ application/views/layouts/topbar.php
@@ -0,0 +1,37 @@
+		<!--**********************************
+            Header start
+        ***********************************-->
+        <div class="header">
+            <div class="header-content">
+                <nav class="navbar navbar-expand">
+                    <div class="collapse navbar-collapse justify-content-between">
+                        <div class="header-left">
+							<div class="nav-item">
+								<div class="input-group search-area">
+									<input type="text" class="form-control" placeholder="Search here">
+									<span class="input-group-text"><a href="javascript:void(0)"><i class="flaticon-381-search-2"></i></a></span>
+								</div>
+							</div>
+                        </div>
+                        <ul class="navbar-nav header-right">
+							
+							<li class="nav-item dropdown  header-profile">
+								<a class="nav-link" href="javascript:void(0);" role="button" data-bs-toggle="dropdown">
+									<img src="<?php echo base_url('public/assets/images/Pic1.jpg');?>" width="56" alt=""/>
+								</a>
+								<div class="dropdown-menu dropdown-menu-end">
+									<a href="" class="dropdown-item ai-icon">
+										<svg id="icon-logout" xmlns="http://www.w3.org/2000/svg" class="text-danger" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line x1="21" y1="12" x2="9" y2="12"></line></svg>
+										<span class="ms-2">Logout </span>
+									</a>
+								</div>
+							</li>
+							
+                        </ul>
+                    </div>
+				</nav>
+			</div>
+		</div>
+        <!--**********************************
+            Header end ti-comment-alt
+        ***********************************-->
