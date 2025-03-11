d9aec85610f304e28aa68c2ba3e19bd6f8d16688
Your Name <msksurjith@gmail.com>
2025-03-11 18:19:16 +0530

    sidebar changes

---
+++ application/views/layouts/sidebar.php
@@ -0,0 +1,52 @@
+<style>
+	.metismenu li ul {
+		margin-left:10px !important;
+	}
+	</style>
+
+<!--**********************************
+            Sidebar start
+        ***********************************-->
+        <div class="deznav">
+			<div class="deznav-scroll">
+			  <ul class="metismenu" id="menu">
+				<li>
+				  <a class="has-arrow ai-icon" href="<?php echo base_url('dashboard'); ?>" aria-expanded="false">
+					<i class="flaticon-013-checkmark"></i>
+					<span class="nav-text">Dashboard</span>
+				  </a>
+				</li>
+				<li>
+				  <a class="has-arrow ai-icon" href="<?php echo base_url('hostel'); ?>" aria-expanded="false">
+					<i class="flaticon-025-dashboard sidemenu-icon"></i>
+					<span class="nav-text">Hostel</span>
+				  </a>
+				  </li>
+				<li>
+				  <a class="has-arrow ai-icon" href="<?php echo base_url('room'); ?>" aria-expanded="false">
+					<i class="flaticon-050-info  sidemenu-icon"></i>
+					<span class="nav-text">Hostel Room</span>
+				  </a>
+				</li>
+				<li>
+				  <a class="has-arrow ai-icon" href="<?php echo base_url('student'); ?>" aria-expanded="false">
+					<i class="flaticon-041-graph sidemenu-icon"></i>
+					<span class="nav-text">Students</span>
+				  </a>
+				</li>
+				<li>
+				  <a class="has-arrow ai-icon" href="<?php echo base_url('attendance'); ?>" aria-expanded="false">
+					<i class="flaticon-086-star sidemenu-icon"></i>
+					<span class="nav-text">Attendance</span>
+				  </a>
+				  </li>
+				  <li>
+				  <a class="has-arrow ai-icon" href="<?php echo base_url('report'); ?>" aria-expanded="false">
+					<i class="flaticon-050-info sidemenu-icon"></i>
+					<span class="nav-text">Report</span>
+				  </a>
+				  </li>
+				
+			  </ul>
+			</div>
+		  </div>
